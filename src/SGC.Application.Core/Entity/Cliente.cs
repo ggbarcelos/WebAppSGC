8b10d8c68598f52eea295eb30997647a06e77901
Glauber Gomes Barcelos <glauberbarcelos@hotmail.com>
2018-08-25 21:10:34 -0300

    First commit

---
+++ src/SGC.Application.Core/Entity/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.Application.Core.Entity
+{
+	public class Cliente
+	{
+		public int ClienteId { get; set; }
+		public string Nome { get; set; }
+		public string CPF { get; set; }
+	}
+}
