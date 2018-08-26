0f572b5804e333ae6c29edce57299daca2d3b0ad
Glauber Gomes Barcelos <glauberbarcelos@hotmail.com>
2018-08-25 22:43:15 -0300

    Cria a classe DataContext e adiciona migrations

---
+++ src/SGC.Infra/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGC.Application.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGC.Infra.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options)
+			:base(options)
+		{
+
+		}
+
+		public DbSet<Cliente> Clientes { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Cliente>().ToTable("Cliente");
+		}
+	}
+}
